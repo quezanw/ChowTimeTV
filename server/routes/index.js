2f30f211282900c326b76a713a3b1980fbd7bb97
quezan <quezanw@gmail.com>
2019-06-07 09:30:24 -0700

    test gitignore

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var snoowrap = require('snoowrap');
-var config = require('../../config');
+var config = require('../../config')
 
 
 const r = new snoowrap({
